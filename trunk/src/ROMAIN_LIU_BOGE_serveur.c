461c05430e0cf566e7c3baf815dba4568d1911ec
ayaouvi2 <ayaouvi2@94931716-6eb6-fd3d-0f03-a8aae382237c>
2010-04-01 22:45:28 +0000

    duel implemente et compile, read tj blocant donc non teste (trop fatigue:()

--- trunk/src/ROMAIN_LIU_BOGE_serveur.c
+++ trunk/src/ROMAIN_LIU_BOGE_serveur.c
@@ -9,11 +9,12 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <regex.h>
 
 
 
 #define TAILLE_MAX_DONNEES  1024
-
+#define CHAINE_MAX 100
 
 
 
@@ -21,7 +22,156 @@ int ia(int fd_client){
 	return EXIT_SUCCESS;
 }
 
+// procedure de jeu contre un autre joueur
 int duel(int fd_client, char* adversaire, int* tube, char* role){
+	
+	int lc, tours_restant = 12;
+	bool termine = false;
+	char* chaine;
+	char* message = malloc(CHAINE_MAX*sizeof(char));
+	regex_t regex_essai;
+	regex_t regex_notation;
+	
+	
+		
+	// on prepare la regex de control du format
+	if (regcomp (&regex_essai, "^[0-9]{5}$", REG_NOSUB) == 0) {
+	
+		// client devant chercher une solution
+		if (strcmp(role,"chercheur") == 0) {
+			// on attend que le colleur ait choisit une combinaison
+			read(tube[0], message, CHAINE_MAX);
+			while (strcmp(message, "combinaison_ok") != 0)
+				read(tube[0], message, CHAINE_MAX);
+		
+			// on informe le chercheur que la combinaison est choisit
+			write(fd_client, message, strlen(message));
+	
+			// tant que le colleur ne dit pas que la combinaisons
+			// est trouve, on transmet les messages en verifiant le format
+			while ( !termine ) {
+				lc = read (fd_client, message, CHAINE_MAX);	
+				message[lc] = '\0';
+			
+				// format correct
+				if (regexec(&regex_essai, message, 0, NULL, 0) == 0) {
+				
+					// transmition
+					write(tube[1], message, strlen(message));
+					tours_restant--;
+					sleep(1);
+				
+					// attente reponse
+					read(tube[0], message, CHAINE_MAX);
+				
+					// analyse de la reponse
+					if (strcmp(message, "gagne") == 0 || tours_restant == 0) {
+						termine = true;
+					}
+				
+					// transmition
+					write(fd_client, message, strlen(message));
+					sleep(1);
+				
+				} else {
+					write(fd_client, "erreur de format du message : le code doit être une suite de 5 chiffres", 72);
+				}
+			}
+		
+			// gagne
+			if (strcmp(message, "gagne") == 0) {
+				sprintf(chaine, "BRAVO ! vous avez trouve en %i essais", 12 - tours_restant);
+				write(fd_client, chaine, strlen(chaine));
+			}
+		
+			//nombre de tours max ecoule
+			else {
+				// recuperation du code
+				read(tube[0], message, CHAINE_MAX);
+				sprintf(chaine, "dommage :(, vous n'avez pas reussi a trouver %s", message);
+				write(fd_client, chaine, strlen(chaine));
+			}
+		}
+	
+		// client devant proposer une combinaison
+		else {
+			char* combinaison = malloc(CHAINE_MAX*sizeof(char));
+			
+			// on demande la combinaison
+			write(fd_client, "combinaison", 12);
+			lc = read (fd_client, combinaison, CHAINE_MAX);	
+			combinaison[lc] = '\0';
+			
+			// tant que le format est incorrect, on redemande
+			while (regexec(&regex_essai, combinaison, 0, NULL, 0) != 0) {
+				write(fd_client, "combinaison", 12);
+				lc = read (fd_client, combinaison, CHAINE_MAX);	
+				combinaison[lc] = '\0';
+			}
+			
+			// on informe le chercheur que la combinaison est choisit
+			write(tube[1], "combinaison_ok", 14);
+			
+			// on prepare la regex de control du format de notation
+			if (regcomp (&regex_notation, "^[VBR]{5}$", REG_NOSUB) == 0) {
+			
+				// tant que le colleur ne dit pas que la combinaisons
+				// est trouve, on transmet les messages en verifiant le format
+				while ( !termine ) {
+					read (tube[0], message, CHAINE_MAX);
+					
+					// transmition
+					write(fd_client, message, strlen(message));
+					tours_restant--;
+					sleep(1);
+					
+					// attente reponse
+					read(fd_client, message, CHAINE_MAX);
+			
+					// on redemande tant que le format est incorrect
+					while (regexec(&regex_notation, message, 0, NULL, 0) != 0 && strcmp(message, "gagne") != 0) {
+						write(fd_client, "erreur de format du message : le code doit être une suite de 5 caracteres apartenant a {V,B,R}", 95);
+						sleep(1);
+						read(fd_client, message, CHAINE_MAX);
+					}
+					
+					// analyse de la reponse
+					if (strcmp(message, "gagne") == 0 || tours_restant == 0) {
+						termine = true;
+					}
+				
+					// transmition
+					write(tube[1], message, strlen(message));
+					sleep(1);
+				}
+		
+				// gagne
+				if (strcmp(message, "gagne") == 0) {
+					sprintf(chaine, "%s a trouve le code en %i essais", adversaire, 12 - tours_restant);
+					write(fd_client, chaine, strlen(chaine));
+				}
+		
+				//nombre de tours max ecoule
+				else {
+					// recuperation du code
+					write(tube[1], combinaison, 6);
+					sprintf(chaine, "BRAVO ! Vous avez reussi a coller %s", adversaire);
+					write(fd_client, chaine, strlen(chaine));
+				}
+			} else {
+				printf("erreur lors de la création de la regex de control des notations");
+			}
+	
+			// on libere la memoire utilisee pour la regex
+			regfree(&regex_notation);
+		}
+	} else {
+		printf("erreur lors de la création de la regex de control des essais");
+	}
+	
+	// on libere la memoire utilisee pour la regex
+	regfree(&regex_essai);
+	
 	return EXIT_SUCCESS;
 }
 
@@ -38,7 +188,7 @@ void fils(int fd_client, int* tube_pub){
 	struct sockaddr adr_src;
 	socklen_t lg_adr_cli;
 	
-	char* message = malloc(50*sizeof(char));
+	char* message = malloc(CHAINE_MAX*sizeof(char));
 	//char* message_reponse = malloc(100*sizeof(char));
 	char* pseudo_adv = malloc(15*sizeof(char));
 	char* pseudo = malloc(15*sizeof(char));
@@ -47,12 +197,12 @@ void fils(int fd_client, int* tube_pub){
 	char* role_adv;
 	
 	// recuperation du pseudo
-	lc = read (fd_client, pseudo, strlen(pseudo));	
+	lc = read (fd_client, pseudo, 15);	
 	pseudo[lc] = '\0';		
 	
 	printf("joueur= %s\n",pseudo);
 	// reception du mode de jeu
-	lc = read (fd_client,message, strlen(message));	
+	lc = read (fd_client,message, CHAINE_MAX);	
 	message[lc] = '\0';
 	
 	// prise en compte de la demande de fermeture
@@ -61,14 +211,14 @@ void fils(int fd_client, int* tube_pub){
 	
 	// jeu solo
 	else if (strcmp(message,"solo") == 0){
-		ia(fd_client);
 		printf("message recu== solo\n");
+		ia(fd_client);
 	}
 	// jeu en duel
 	else if (strcmp(message,"duel") == 0) {
 		printf("message recu== duel\n");
 		// on regarde s'il y a un joueur de disponible
-		if (read(tube_pub[0], message, 50) == 0) {	// aucun joueur disponible
+		if (read(tube_pub[0], message, CHAINE_MAX) == 0) {	// aucun joueur disponible
 			// on cree un tube prive
 			if (pipe(tube_priv) != 0) {
 				perror("pipe");
@@ -87,7 +237,7 @@ void fils(int fd_client, int* tube_pub){
 		
 			// on recupere les preferences de jeu,
 			// et on attend un autre joueur
-			lc = recvfrom (fd_client, message, 50, 0, &adr_src, &lg_adr_cli);
+			lc = recvfrom (fd_client, message, CHAINE_MAX, 0, &adr_src, &lg_adr_cli);
 			message[lc] = '\0';
 			if (strcmp(message,"chercheur") == 0) {	// chercheur
 				sprintf(role, "%s", message);
